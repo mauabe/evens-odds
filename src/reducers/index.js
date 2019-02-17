8e961c28a5c5d9ceb07307b6618f0a8f7de1ea46
Mauricio Feldman-Abe <mauabe@gmx.com>
2019-02-17 00:26:16 -0800

    fixing reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,8 @@
-import { 
-  SET_GAME_STARTED, 
-  SET_INSTRUCTIONS_EXPANDED, 
-  FETCH_DECK_RESULT 
-} from '../actions/types';
+import { combineReducers } from 'redux';
+import settingsReducer from './settings';
+import deckReducer from './deck';
 
-
-const DEFAULT_SETTINGS = {
-  gameStarted: false,
-  instructionsExpanded: false
-}
-
-const rootReducer = (state = DEFAULT_SETTINGS, action) => {
-  // console.log('state', state, 'action', action)
-
-  switch(action.type){
-    case SET_GAME_STARTED:
-      return{ ...state, gameStarted: action.gameStarted}
-        //instructionsExpanded: state.instructionsExpanded
-    case SET_INSTRUCTIONS_EXPANDED:
-      return{ ...state, instructionsExpanded: action.instructionsExpanded}
-        //gameStarted: state.gameStarted,
-    case FETCH_DECK_RESULT:
-    return { ...state, remaining, deck_id };
-    default: 
-      return state;
-  }
-}
-export default rootReducer;
+export default combineReducers({
+  settings: settingsReducer,
+  deck: deckReducer
+})
---
+++ src/reducers/settings.js
@@ -0,0 +1,21 @@
+import { SET_GAME_STARTED, SET_INSTRUCTIONS_EXPANDED } from '../actions/types';
+
+
+const DEFAULT_SETTINGS = {
+  gameStarted: false,
+  instructionsExpanded: false
+}
+
+const settingsReducer = (state = DEFAULT_SETTINGS, action) => {
+  switch(action.type){
+    case SET_GAME_STARTED:
+      return{ ...state, gameStarted: action.gameStarted}
+        //instructionsExpanded: state.instructionsExpanded
+    case SET_INSTRUCTIONS_EXPANDED:
+      return{ ...state, instructionsExpanded: action.instructionsExpanded}
+        //gameStarted: state.gameStarted,
+    default: 
+      return state;
+  }
+};
+export default settingsReducer;
